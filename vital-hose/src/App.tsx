86887ecd6386b921c82d1d38124f83a8fd94befa
jkumari25 <jagriti.sj15@gmail.com>
2023-03-03 13:18:49 +0530

    design of homepage

--- vital-hose/src/App.tsx
+++ vital-hose/src/App.tsx
@@ -1,15 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import VideoQuizGame from './Components/VideoQuizGame'
+import MainRoutes from './Routes/MainRoutes'
 
 function App() {
   const [count, setCount] = useState(0)
 
-
   return (
     <div className="App" >
-      <VideoQuizGame/>
+      <MainRoutes />
     </div>
   )
 }
---
+++ vital-hose/src/Routes/MainRoutes.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Routes,Route} from "react-router-dom";
+import Home from '../Components/Home/Home';
+import Instruction from '../Components/Instruction/Instruction';
+import VideoQuizGame from '../Components/VideoQuizGame';
+
+const MainRoutes = () => {
+  return (
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/instruction" element={<Instruction />} />
+        <Route path="/video" element={<VideoQuizGame />} />
+    </Routes>
+  )
+}
+
+export default MainRoutes
