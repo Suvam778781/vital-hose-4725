323d733292577144dae8952660bef81ff775230c
Moumita Das <dasmoumita.me@gmail.com>
2023-03-03 15:26:05 +0530

    timer

--- vital-hose/src/App.tsx
+++ vital-hose/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+// import Timmer from "./Components/timmer/Timmer";
 import VideoQuizGame from './Components/VideoQuizGame'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
   return (
     <div className="App" >
       <VideoQuizGame/>
+
     </div>
   )
 }
